8dfee2df2c0176cf1d6525fed8c5e52b7d67ad9e
seonjae <seonjae9488@gmail.com>
2022-06-21 13:32:55 +0900

    api 통신 promise에서 async await방식으로 변경

--- vue-news/src/api/index.js
+++ vue-news/src/api/index.js
@@ -8,8 +8,12 @@ function fetchNewsList(){
     return axios.get(`${config.baseUrl}news/1.json`);
 }
 
-function fetchAskList(){
-    return axios.get(`${config.baseUrl}ask/1.json`);
+async function fetchAskList(){
+    try {
+        return axios.get(`${config.baseUrl}ask/1.json`);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function fetchJobsList(){
@@ -17,15 +21,27 @@ function fetchJobsList(){
 }
 
 function fetchList(pageName){
-    return axios.get(`${config.baseUrl}${pageName}/1.json`);
+    try {
+        return axios.get(`${config.baseUrl}${pageName}/1.json`);
+    } catch (error) {
+        console.log(error);
+    }  
 }
 
 function fetchUserInfo(username){
-    return axios.get(`${config.baseUrl}user/${username}.json`);
+    try {
+        return axios.get(`${config.baseUrl}user/${username}.json`);
+    } catch (error) {
+        console.log(error);
+    } 
 }
 
 function fetchItemInfo(itemid){
-    return axios.get(`${config.baseUrl}item/${itemid}.json`);
+    try {
+        return axios.get(`${config.baseUrl}item/${itemid}.json`);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -1,60 +1,50 @@
 import { fetchNewsList, fetchJobsList, fetchAskList, fetchList, fetchUserInfo, fetchItemInfo } from '../api/index.js';
 
 export default {
-    FETCH_NEWS(context) {
-      return fetchNewsList()
-          .then(response => {
-              context.commit('SET_NEWS', response.data);
-              return response;
-          })
-          .catch(error => {
-              console.log(error);
-          })
+    //promise
+    // FETCH_NEWS(context) {
+    //   return fetchNewsList()
+    //       .then(response => {
+    //           context.commit('SET_NEWS', response.data);
+    //           return response;
+    //       })
+    //       .catch(error => {
+    //           console.log(error);
+    //       })
+    // },
+    //async
+    async FETCH_NEWS(context) {
+        const response = await fetchNewsList();
+        context.commit('SET_NEWS', response.data);
+        return response;
     },
-    FETCH_JOBS({ commit }) {
-        return fetchJobsList()
-         .then(({ data }) => {
-             commit('SET_JOBS', data);
-         })
-         .catch(error => {
-             console.log(error);
-         })
-    },
-    FETCH_ASK({ commit }) {
-        return fetchAskList()
-          .then(({ data }) => {
-              commit('SET_ASK', data);
-          })
-          .catch(error => {
-              console.log(error);
-          })
-    },
-    FETCH_USER({ commit }, name) {
-        return fetchUserInfo(name)
-        .then(({ data }) => {
-            commit('SET_USER', data);
-        })
-        .catch(error => {
+    async FETCH_JOBS({ commit }) {
+        try {
+            const response = await fetchJobsList();
+            commit('SET_JOBS', response.data);
+            return response;
+        } catch (error) {
             console.log(error);
-        });
+        }
     },
-    FETCH_ITEM({ commit }, id) {
-        return fetchItemInfo(id)
-        .then(({ data }) => {
-            commit('SET_ITEM', data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    async FETCH_ASK({ commit }) { //api단에서 try catch처리
+        const response = await fetchAskList();
+        commit('SET_JOBS', response.data);
+        return response;
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-        .then(response => {
-            commit('SET_LIST', response.data);
-            return response.data;
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    async FETCH_USER({ commit }, name) {
+        const response = await fetchUserInfo(name)
+        commit('SET_USER', response.data);
+        return response;
+    },
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchItemInfo(id)
+        commit('SET_ITEM', response.data);
+        return response;
+    },
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName)
+        commit('SET_LIST', response.data);
+        return response;
     }
 }
